65d712598a5330126fbb38e459466f8d61c40423
fakestarbaby <fakestarbaby@gmail.com>
2012-02-13 20:26:07 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamplePivotal::Application.config.secret_token = '003fb209b001e98f960f4f69ae054350bb8fb55c6ce0f258ba29f021d30e62ec3ec55a5f24fdef637235618a3c46561a14ac2d64482faa76537719a46de82f79'
